805ac884d0d8c3c0cdb0f72289fff7c5468b85f7
kartik260406 <kartika.26042006@gmail.com>
2026-06-13 17:37:15 +0530

    Polished todo list

--- Week2/todo.py
+++ Week2/todo.py
@@ -7,7 +7,7 @@ def main():
         sys.exit("Too many args")
     if not sys.argv[1].endswith(".txt"):
         sys.exit("Wrong file")
-    valid_input = ["view tasks","add task","mark test done"]
+    valid_input = ["view tasks","add task","mark task done"]
     task = input("Enter the task from (view tasks,add task,mark test done): ").strip().lower()
     file_path = Path(sys.argv[1])
     file_found = True
@@ -20,11 +20,14 @@ def main():
     if task not in valid_input:
         raise ValueError("Wrong task entered")
     if task == valid_input[0]:
-        print(view_task)
+        tasks = view_task()
+        for task in tasks:
+            print(task,end = "")
     elif task == valid_input[1]:
         add_task(file_found)
         print("Task Done")
-    
+    elif task == valid_input[2]:
+        mark_task()
 
 def view_task():
     tasks = []
@@ -33,25 +36,27 @@ def view_task():
             tasks.append(line)
     return tasks
 
-def add_task(*args,file_found):
+def add_task(file_found):
     task_to_add = input("Enter the tasks seperated by commas: ")
-    args = task_to_add.split(",").strip()
+    args = [t.strip() for t in task_to_add.split(",")]
     if file_found:
         work = "a"
     else:
         work = "w"
     with open(sys.argv[1],work) as file:
         for task in args:
-            file.write(task,end = "")
+            file.write(task)
 
 def mark_task():
     all_task = view_task()
     test_to_mark = input("Enter the task to mark: ")
     with open(sys.argv[1],"w") as file:
         for task in all_task:
-            file.write(task,end = "")
-            if task == test_to_mark:
-                file.write("✅",end = "")
+            stripped = task.strip()
+            if stripped == test_to_mark:
+                file.write("✅"+stripped+"\n")
+            else:
+                file.write(task)
 
 if __name__ == "__main__":
     main()
